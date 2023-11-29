df80953dc87c8802b9a6f42f04c0aa8cd8ecfffa
cylexVEVO <28271391+cylexVEVO@users.noreply.github.com>
2023-11-29 15:23:24 -0500

    enable rounded window corners

--- 2.sh
+++ 2.sh
@@ -3,7 +3,7 @@ gnome-extensions enable appindicatorsupport@rgcjonas.gmail.com
 gnome-extensions enable quick-settings-audio-panel@rayzeq.github.io
 gnome-extensions enable just-perfection-desktop@just-perfection
 gnome-extensions enable transparent-top-bar@ftpix.com
-gnome-extensions enable transparent-top-bar@ftpix.com
+gnome-extensions enable rounded-window-corners@yilozt
 
 rm ext1.zip ext2.zip ext3.zip ext4.zip ext5.zip
 
